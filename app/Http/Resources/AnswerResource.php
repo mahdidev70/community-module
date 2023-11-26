16cdad0a6bf848c573ea03838428c07e0e09f933
AmirMahdi-for <amirmahdifor@gmail.com>
2023-11-26 19:33:26 +0330

    add resources file

---
+++ app/Http/Resources/AnswerResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\Community;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AnswerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'question' => [
+                'id' => $this->question->id,
+                'title' => $this->question->text,
+                'slug' =>$this->question->slug,
+            ],
+            'author' => [
+                'id' => $this->user->id, 
+                'displayName' => $this->user->getDisplayName(), 
+                'avatarUrl' => $this->user->avatar_url, 
+            ],
+            'title' => $this->text,
+            'status' => $this->status,
+            'creationDate' => $this->created_at,
+            'likesCount' => $this->likes_count ?? 0,
+            'dislikesCount' => $this->dislikes_count ?? 0,
+        ];
+    }
+}
---
+++ app/Http/Resources/QuestionResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources\Community;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->text,
+            'creationDate' =>$this->created_at,
+            'category' => [
+                'title' => $this->category->title,
+                'slug' => $this->category->slug,
+            ],
+            'author' => [
+                'id' => $this->asker->id,
+                'displayName' => $this->asker->getDisplayName(),
+                'avatarUrl' => $this->asker->avatar_url,
+            ],
+            'likesCount' => $this->likes_count ?? 0,
+            'dislikesCount' => $this->dislikes_count ?? 0,
+            'answersCount' => $this-> allAnswers->count(),
+            'attachments' => $this->attachments,
+            'publicationDate' => $this->publication_date,
+            'viewsCount' => $this->viewsCount,
+            'status' => $this->status,
+        ];
+    }
+}

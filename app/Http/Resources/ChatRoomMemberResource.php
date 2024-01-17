8c95f381219ccef082c00e12da22cd3b1a06b84c
Amirmahdi-for <amirmahdifor@gmail.com>
2024-01-17 12:17:41 +0330

    add new file for member of rooms

---
+++ app/Http/Resources/ChatRoomMemberResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TechStudio\Community\app\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChatRoomMemberResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'displayName' => $this->getDisplayName(), 
+            'avatarUrl' => $this->avatar_url,
+        ];
+    }
+}

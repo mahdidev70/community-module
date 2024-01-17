6fcec36b122effd5550a66b07dc6a572df787ca4
Amirmahdi-for <amirmahdifor@gmail.com>
2024-01-17 12:19:17 +0330

    add new changes for room resource

--- app/Http/Resources/ChatRoomResource.php
+++ app/Http/Resources/ChatRoomResource.php
@@ -23,11 +23,16 @@ class ChatRoomResource extends JsonResource
             'status' => $this->status,
             'avatarUrl' => $this->avatar_url,
             'bannerUrl' => $this->banner_url,
-            'creationDate' =>$this->created_at,
+            'creationDate' => $this->created_at,
+            'maxMember' => $this-> max_member,
+            'memberCount' => $this->members->count(),
+            'joinLink' => route('join.chatroom').'/'.$this->slug,
             'category' => [
+                'id' => $this->category->id,
                 'title' => $this->category->title,
                 'slug' => $this->category->slug,
             ],
+            'members' => ChatRoomMemberResource::collection($this->members),
         ];
     }
 }

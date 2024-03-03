9f2c574993734b3097ca9b2511a9d0ae59b7e978
next <mahdidev70@gmail.com>
2024-03-03 16:26:49 +0330

    fix: add join link

--- app/Http/Resources/ChatRoomResource.php
+++ app/Http/Resources/ChatRoomResource.php
@@ -26,7 +26,7 @@ class ChatRoomResource extends JsonResource
             'creationDate' => $this->created_at,
             'maxMember' => $this-> max_member,
             'memberCount' => $this->members->count(),
-            'joinLink' => $this->join_link /*route('join.chatroom').'/'.$this->slug*/,
+            'joinLink' => env('CLIENT_APP_URL','url') . $this->join_link /*route('join.chatroom').'/'.$this->slug*/,
             'categoryId' => $this->category->id ?? null,
             'category' => [
                 'id' => $this->category->id ?? null,
